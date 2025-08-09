d59df17862dd28b14b0c2c37f2f85e0584f25ae0
Irudika02 <iriyaaa12345@gmail.com>
2025-08-09 19:45:03 +0530

    added product class

---
+++ src/main/java/com/shop/retailpos/model/Product.java
@@ -0,0 +1,21 @@
+package com.shop.retailpos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+
+    // Getters and Setters
+}
+
