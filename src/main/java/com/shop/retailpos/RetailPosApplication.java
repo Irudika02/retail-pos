41cfbc5d34c27e534d992da4cdbb57f517d9b1fa
Irudika02 <iriyaaa12345@gmail.com>
2025-08-09 17:04:45 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/shop/retailpos/RetailPosApplication.java
@@ -0,0 +1,13 @@
+package com.shop.retailpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailPosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/retailpos/RetailPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.retailpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
