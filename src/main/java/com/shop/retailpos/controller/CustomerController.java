082307dabf2e529448fe27700ca0c7b26078a748
Irudika02 <iriyaaa12345@gmail.com>
2025-08-10 10:05:35 +0530

    customer controller added

--- src/main/java/com/shop/retailpos/controller/CustomerController.java
+++ src/main/java/com/shop/retailpos/controller/CustomerController.java
@@ -1,4 +1,44 @@
 package com.shop.retailpos.controller;
 
+import com.shop.retailpos.model.Customer;
+import com.shop.retailpos.service.CustomerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomers(){
+        return customerService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public Customer getCustomerById(@PathVariable Long id){
+        return customerService.get(id);
+    }
+
+    @PostMapping
+    public Customer addCustomer(@RequestBody Customer customer){
+        return customerService.save(customer);
+    }
+
+    @PutMapping("/{id}")
+    public Customer updateCustomer(@PathVariable Long id, @RequestBody Customer customer){
+        customer.setId(id);
+        return customerService.save(customer);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable Long id){
+        customerService.delete(id);
+    }
 }
