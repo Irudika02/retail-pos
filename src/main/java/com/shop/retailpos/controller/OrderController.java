6a64ca410179bcc808abb262bf9013e1a77fb4f6
Irudika02 <iriyaaa12345@gmail.com>
2025-08-10 10:06:50 +0530

    Order controller added

--- src/main/java/com/shop/retailpos/controller/OrderController.java
+++ src/main/java/com/shop/retailpos/controller/OrderController.java
@@ -1,4 +1,45 @@
 package com.shop.retailpos.controller;
 
+import com.shop.retailpos.model.Order;
+import com.shop.retailpos.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService){
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders(){
+        return orderService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public Order getOrderById(@PathVariable Long id){
+        return orderService.get(id);
+    }
+
+    @PostMapping
+    public Order addOrder(@RequestBody Order order){
+        return orderService.save(order);
+    }
+
+    @PutMapping("/{id}")
+    public Order updateOrder(@PathVariable Long id, @RequestBody Order order){
+        order.setId(id);
+        return orderService.save(order);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOrder(@PathVariable Long id){
+        orderService.delete(id);
+    }
 }
+
